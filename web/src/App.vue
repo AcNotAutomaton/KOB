a82a557339202ac6ddbeb4ab0d245beb61c76a67
AcNotAutomaton <550578884@qq.com>
2022-09-12 14:18:04 +0800

    项目骨架

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+
+  </div>
+  <router-view></router-view>
 </template>
 
+<script>
+
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+body {
+  background-image: url("../src/assets/1.jpeg");
+  background-size: cover;
 }
 </style>
