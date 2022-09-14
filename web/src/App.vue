9fd0666a9a7ec0bbda8aea7bfbd8522454587b28
AcNotAutomaton <550578884@qq.com>
2022-09-14 17:12:46 +0800

    实现对战页面和障碍物

--- web/src/App.vue
+++ web/src/App.vue
@@ -17,7 +17,7 @@ export default {
 
 <style>
 body {
-  background-image: url("../src/assets/1.jpeg");
+  background-image: url("../src/assets/images/1.jpeg");
   background-size: cover;
 }
 </style>
