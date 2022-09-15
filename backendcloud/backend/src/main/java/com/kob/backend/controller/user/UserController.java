849a0745df43b91b7c8d346cf0714c5122301643
AcNotAutomaton <550578884@qq.com>
2022-09-15 09:24:38 +0800

    MySQL 50%

---
+++ backendcloud/backend/src/main/java/com/kob/backend/controller/user/UserController.java
@@ -0,0 +1,21 @@
+package com.kob.backend.controller.user;
+
+import com.kob.backend.mapper.UserMapper;
+import com.kob.backend.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserMapper userMapper;
+
+    @GetMapping("/user/all")
+    public List<User> getAll() {
+        return userMapper.selectList(null);
+    }
+}
---
+++ backendcloud/backend/src/main/java/com/kob/backend/pojo/User.java
@@ -0,0 +1,16 @@
+package com.kob.backend.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+    private String photo;
+    private Integer rating;
+}
