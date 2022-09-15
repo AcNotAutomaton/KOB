492e2f668f99adf7bb1ce19fb81b12b156291ca2
AcNotAutomaton <550578884@qq.com>
2022-09-15 14:43:00 +0800

    backend MySQL 明文登录

---
+++ backendcloud/backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
@@ -0,0 +1,13 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "pk/index.html";
+    }
+}
