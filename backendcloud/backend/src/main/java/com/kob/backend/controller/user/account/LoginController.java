796528f6c7557d2d22ab4ca4d0f5a4fc848072b7
AcNotAutomaton <550578884@qq.com>
2022-09-16 22:21:40 +0800

    JWT验证 LoginService添加

---
+++ backendcloud/backend/src/main/java/com/kob/backend/controller/user/account/LoginController.java
@@ -0,0 +1,21 @@
+package com.kob.backend.controller.user.account;
+
+import com.kob.backend.service.user.account.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+public class LoginController {
+
+    @Autowired
+    private LoginService loginService;
+
+    @PostMapping("/api/user/account/token")
+    public Map<String, String> getToken(@RequestParam Map<String, String> map) {
+        String username = map.get("username");
+        String password = map.get("password");
+        return loginService.getToken(username, password);
+    }
+}
