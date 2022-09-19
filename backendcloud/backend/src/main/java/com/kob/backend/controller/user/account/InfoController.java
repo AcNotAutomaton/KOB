f92a62ba029b0f1e6cf61cd5da6e59618af04221
AcNotAutomaton <550578884@qq.com>
2022-09-19 11:13:20 +0800

    backend 后端完成

--- backendcloud/backend/src/main/java/com/kob/backend/controller/user/account/InfoController.java
+++ backendcloud/backend/src/main/java/com/kob/backend/controller/user/account/InfoController.java
@@ -1,5 +1,17 @@
 package com.kob.backend.controller.user.account;
 
-public class InfoController {
+import com.kob.backend.service.user.account.InfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+
+public class InfoController {
+    @Autowired
+    private InfoService infoService;
+
+    @GetMapping("/api/user/account/info")
+    public Map<String, String> getinfo() {
+        return infoService.getinfo();
+    }
 }
---
+++ backendcloud/backend/src/main/java/com/kob/backend/controller/user/account/RegisterController.java
@@ -0,0 +1,21 @@
+package com.kob.backend.controller.user.account;
+
+import com.kob.backend.service.user.account.RegisterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+public class RegisterController {
+    @Autowired
+    private RegisterService registerService;
+
+    @PostMapping("/api/user/account/register")
+    public Map<String, String> register(@RequestParam Map<String, String> map) {
+        String username = map.get("username");
+        String password = map.get("password");
+        String confirmedPassword = map.get("confirmedPassword");
+        return registerService.register(username, password, confirmedPassword);
+    }
+}
