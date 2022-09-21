2e5cfeeae83ec553f66cd4e328e6bca411ab01d6
cuiliuyang <550578884@qq.com>
2022-09-21 19:34:53 +0800

    修复后端BUG

--- backendcloud/backend/src/main/java/com/kob/backend/config/SecurityConfig.java
+++ backendcloud/backend/src/main/java/com/kob/backend/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/user/account/token/", "/api/user/account/register/").permitAll()
+                .antMatchers("/user/account/token/", "/user/account/register/").permitAll()
                 .antMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyRequest().authenticated();
 
--- backendcloud/backend/src/main/java/com/kob/backend/pojo/User.java
+++ backendcloud/backend/src/main/java/com/kob/backend/pojo/User.java
@@ -15,5 +15,4 @@ public class User {
     private String username;
     private String password;
     private String photo;
-    private Integer rating;
 }
