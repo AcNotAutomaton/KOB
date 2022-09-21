f7c2c89977300af268815b97dfa249f576fb39c8
cuiliuyang <550578884@qq.com>
2022-09-21 20:15:40 +0800

    web detele api

--- web/src/store/user.js
+++ web/src/store/user.js
@@ -31,7 +31,7 @@ export default {
     actions: {
         login(context, data) {
             $.ajax({
-                url: "http://127.0.0.1:3000/api/user/account/token/",
+                url: "http://127.0.0.1:3000/user/account/token/",
                 type: "post",
                 data: {
                     username: data.username,
@@ -52,7 +52,7 @@ export default {
         },
         getinfo(context, data) {
             $.ajax({
-                url: "http://127.0.0.1:3000/api/user/account/info/",
+                url: "http://127.0.0.1:3000/user/account/info/",
                 type: "get",
                 headers: {
                     Authorization: "Bearer " + context.state.token,
