b446c65890fe7820febd8157d81e7c92f4542e08
cuiliuyang <550578884@qq.com>
2022-09-24 14:01:28 +0800

    Web 实现pk的匹配界面

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -3,84 +3,36 @@ import { Wall } from "./Wall";
 import { Snake } from './Snake';
 
 export class GameMap extends AcGameObject {
-    constructor(ctx, parent) {
+    constructor(ctx, parent, store) {
         super();
 
         this.ctx = ctx;
         this.parent = parent;
+        this.store = store;
         this.L = 0;
 
         this.rows = 13;
         this.cols = 14;
-
+        
         this.inner_walls_count = 20;
         this.walls = [];
 
         this.snakes = [
-            new Snake({ id: 0, color: "#4876EC", r: this.rows - 2, c: 1 }, this),
-            new Snake({ id: 1, color: "#F94848", r: 1, c: this.cols - 2 }, this),
+            new Snake({id: 0, color: "#4876EC", r: this.rows - 2, c: 1}, this),
+            new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2}, this),
         ];
     }
 
-    check_connectivity(g, sx, sy, tx, ty) {
-        if (sx == tx && sy == ty) return true;
-        g[sx][sy] = true;
-
-        let dx = [-1, 0, 1, 0], dy = [0, 1, 0, -1];
-        for (let i = 0; i < 4; i++) {
-            let x = sx + dx[i], y = sy + dy[i];
-            if (!g[x][y] && this.check_connectivity(g, x, y, tx, ty))
-                return true;
-        }
-
-        return false;
-    }
-
     create_walls() {
-        const g = [];
-        for (let r = 0; r < this.rows; r++) {
-            g[r] = [];
-            for (let c = 0; c < this.cols; c++) {
-                g[r][c] = false;
-            }
-        }
+        const g = this.store.state.pk.gamemap;
 
-        // 给四周加上障碍物
-        for (let r = 0; r < this.rows; r++) {
-            g[r][0] = g[r][this.cols - 1] = true;
-        }
-
-        for (let c = 0; c < this.cols; c++) {
-            g[0][c] = g[this.rows - 1][c] = true;
-        }
-
-        // 创建随机障碍物
-        for (let i = 0; i < this.inner_walls_count / 2; i++) {
-            for (let j = 0; j < 1000; j++) {
-                let r = parseInt(Math.random() * this.rows);
-                let c = parseInt(Math.random() * this.cols);
-                if (g[r][c] || g[this.rows - 1 - r][this.cols - 1 - c]) continue;
-                if (r == this.rows - 2 && c == 1 || r == 1 && c == this.cols - 2)
-                    continue;
-
-                g[r][c] = g[this.rows - 1 - r][this.cols - 1 - c] = true;
-                break;
-            }
-        }
-
-        const copy_g = JSON.parse(JSON.stringify(g));
-        if (!this.check_connectivity(copy_g, this.rows - 2, 1, 1, this.cols - 2))
-            return false;
-
-        for (let r = 0; r < this.rows; r++) {
-            for (let c = 0; c < this.cols; c++) {
+        for (let r = 0; r < this.rows; r ++ ) {
+            for (let c = 0; c < this.cols; c ++ ) {
                 if (g[r][c]) {
                     this.walls.push(new Wall(r, c, this));
                 }
             }
         }
-
-        return true;
     }
 
     add_listening_events() {
@@ -100,10 +52,8 @@ export class GameMap extends AcGameObject {
     }
 
     start() {
-        for (let i = 0; i < 1000; i++)
-            if (this.create_walls())
-                break;
-
+        this.create_walls();
+        
         this.add_listening_events();
     }
 
@@ -136,9 +86,9 @@ export class GameMap extends AcGameObject {
         for (const snake of this.snakes) {
             let k = snake.cells.length;
             if (!snake.check_tail_increasing()) {  // 当蛇尾会前进的时候，蛇尾不要判断
-                k--;
+                k -- ;
             }
-            for (let i = 0; i < k; i++) {
+            for (let i = 0; i < k; i ++ ) {
                 if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
                     return false;
             }
@@ -157,8 +107,8 @@ export class GameMap extends AcGameObject {
 
     render() {
         const color_even = "#AAD751", color_odd = "#A2D149";
-        for (let r = 0; r < this.rows; r++) {
-            for (let c = 0; c < this.cols; c++) {
+        for (let r = 0; r < this.rows; r ++ ) {
+            for (let c = 0; c < this.cols; c ++ ) {
                 if ((r + c) % 2 == 0) {
                     this.ctx.fillStyle = color_even;
                 } else {
--- web/src/components/GameMap.vue
+++ web/src/components/GameMap.vue
@@ -5,16 +5,18 @@
 </template>
 
 <script>
-import { GameMap } from "../assets/scripts/GameMap";
+import { GameMap } from "@/assets/scripts/GameMap";
 import { ref, onMounted } from 'vue'
+import { useStore } from "vuex";
 
 export default {
     setup() {
+        const store = useStore();
         let parent = ref(null);
         let canvas = ref(null);
 
         onMounted(() => {
-            new GameMap(canvas.value.getContext('2d'), parent.value)
+            new GameMap(canvas.value.getContext('2d'), parent.value, store)
         });
 
         return {
