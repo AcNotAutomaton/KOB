9c28b2bf9a92df03e130d647a28e8ffc736d85d2
cuiliuyang <550578884@qq.com>
2022-09-28 11:17:43 +0800

    bot 代码完成

---
+++ backendcloud/BotRunningSystem/src/main/java/com/kob/botrunningsystem/service/impl/utils/Consumer.java
@@ -0,0 +1,61 @@
+package com.kob.botrunningsystem.service.impl.utils;
+
+import com.kob.botrunningsystem.utils.BotInterface;
+import org.joor.Reflect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+public class Consumer extends Thread {
+    private Bot bot;
+    private static RestTemplate restTemplate;
+    private final static String receiveBotMoveUrl = "http://127.0.0.1:3000/pk/receive/bot/move/";
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        Consumer.restTemplate = restTemplate;
+    }
+
+    public void startTimeout(long timeout, Bot bot) {
+        this.bot = bot;
+        this.start();
+
+        try {
+            this.join(timeout);  // 最多等待timeout秒
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            this.interrupt();  // 终端当前线程
+        }
+    }
+
+    private String addUid(String code, String uid) {  // 在code中的Bot类名后添加uid
+        int k = code.indexOf(" implements com.kob.botrunningsystem.utils.BotInterface");
+        return code.substring(0, k) + uid + code.substring(k);
+    }
+
+    @Override
+    public void run() {
+        UUID uuid = UUID.randomUUID();
+        String uid = uuid.toString().substring(0, 8);
+
+        BotInterface botInterface = Reflect.compile(
+                "com.kob.botrunningsystem.utils.Bot" + uid, 
+                addUid(bot.getBotCode(), uid)
+        ).create().get();
+
+        Integer direction = botInterface.nextMove(bot.getInput());
+        System.out.println("move-direction: " + bot.getUserId() + " " + direction);
+
+        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+        data.add("user_id", bot.getUserId().toString());
+        data.add("direction", direction.toString());
+
+        restTemplate.postForObject(receiveBotMoveUrl, data, String.class);
+    }
+}
