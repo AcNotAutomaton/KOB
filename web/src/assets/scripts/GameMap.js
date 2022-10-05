66ed638cfb0921ce029119600d85fdf6e4626a12
cuiliuyang <550578884@qq.com>
2022-10-05 14:35:02 +0800

    回放界面和功能都有BUG

--- web/src/assets/scripts/GameMap.js
+++ web/src/assets/scripts/GameMap.js
@@ -13,21 +13,21 @@ export class GameMap extends AcGameObject {
 
         this.rows = 13;
         this.cols = 14;
-        
+
         this.inner_walls_count = 20;
         this.walls = [];
 
         this.snakes = [
-            new Snake({id: 0, color: "#4876EC", r: this.rows - 2, c: 1}, this),
-            new Snake({id: 1, color: "#F94848", r: 1, c: this.cols - 2}, this),
+            new Snake({ id: 0, color: "#4876EC", r: this.rows - 2, c: 1 }, this),
+            new Snake({ id: 1, color: "#F94848", r: 1, c: this.cols - 2 }, this),
         ];
     }
 
     create_walls() {
         const g = this.store.state.pk.gamemap;
 
-        for (let r = 0; r < this.rows; r ++ ) {
-            for (let c = 0; c < this.cols; c ++ ) {
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
                 if (g[r][c]) {
                     this.walls.push(new Wall(r, c, this));
                 }
@@ -36,27 +36,53 @@ export class GameMap extends AcGameObject {
     }
 
     add_listening_events() {
-        this.ctx.canvas.focus();
-        
-        this.ctx.canvas.addEventListener("keydown", e => {
-            let d = -1;
-            if (e.key === 'w') d = 0;
-            else if (e.key === 'd') d = 1;
-            else if (e.key === 's') d = 2;
-            else if (e.key === 'a') d = 3;
+        console.log(this.store.state.record);
 
-            if (d >= 0) {
-                this.store.state.pk.socket.send(JSON.stringify({
-                    event: "move",
-                    direction: d,
-                }));
-            }
-        });
+        if (this.store.state.record.is_record) {
+            let k = 0;
+
+            const a_steps = this.store.state.record.a_steps;
+            const b_steps = this.store.state.record.b_steps;
+            const loser = this.store.state.record.record_loser;
+            const [snake0, snake1] = this.snakes;
+            const interval_id = setInterval(() => {
+                if (k >= a_steps.length - 1) {
+                    if (loser === "all" || loser === "A") {
+                        snake0.status = "die";
+                    }
+                    if (loser === "all" || loser === "B") {
+                        snake1.status = "die";
+                    }
+                    clearInterval(interval_id);
+                } else {
+                    snake0.set_direction(parseInt(a_steps[k]));
+                    snake1.set_direction(parseInt(b_steps[k]));
+                }
+                k++;
+            }, 300);
+        } else {
+            this.ctx.canvas.focus();
+
+            this.ctx.canvas.addEventListener("keydown", e => {
+                let d = -1;
+                if (e.key === 'w') d = 0;
+                else if (e.key === 'd') d = 1;
+                else if (e.key === 's') d = 2;
+                else if (e.key === 'a') d = 3;
+
+                if (d >= 0) {
+                    this.store.state.pk.socket.send(JSON.stringify({
+                        event: "move",
+                        direction: d,
+                    }));
+                }
+            });
+        }
     }
 
     start() {
         this.create_walls();
-        
+
         this.add_listening_events();
     }
 
@@ -89,9 +115,9 @@ export class GameMap extends AcGameObject {
         for (const snake of this.snakes) {
             let k = snake.cells.length;
             if (!snake.check_tail_increasing()) {  // 当蛇尾会前进的时候，蛇尾不要判断
-                k -- ;
+                k--;
             }
-            for (let i = 0; i < k; i ++ ) {
+            for (let i = 0; i < k; i++) {
                 if (snake.cells[i].r === cell.r && snake.cells[i].c === cell.c)
                     return false;
             }
@@ -110,8 +136,8 @@ export class GameMap extends AcGameObject {
 
     render() {
         const color_even = "#AAD751", color_odd = "#A2D149";
-        for (let r = 0; r < this.rows; r ++ ) {
-            for (let c = 0; c < this.cols; c ++ ) {
+        for (let r = 0; r < this.rows; r++) {
+            for (let c = 0; c < this.cols; c++) {
                 if ((r + c) % 2 == 0) {
                     this.ctx.fillStyle = color_even;
                 } else {
--- web/src/views/pk/PkIndexView.vue
+++ web/src/views/pk/PkIndexView.vue
@@ -2,6 +2,14 @@
     <PlayGround v-if="$store.state.pk.status === 'playing'" />
     <MatchGround v-if="$store.state.pk.status === 'matching'" />
     <ResultBoard v-if="$store.state.pk.loser != 'none'" />
+    <div class="user-color"
+        v-if="$store.state.pk.status === 'playing' && parseInt($store.state.user.id) === parseInt($store.state.pk.a_id)">
+        左下角
+    </div>
+    <div class="user-color"
+        v-if="$store.state.pk.status === 'playing' && parseInt($store.state.user.id) === parseInt($store.state.pk.b_id)">
+        右上角
+    </div>
 </template>
 
 <script>
@@ -22,6 +30,7 @@ export default {
         const socketUrl = `ws://127.0.0.1:3000/websocket/${store.state.user.token}/`;
 
         store.commit("updateLoser", "none");
+        store.commit("updateIsRecord", false);
 
         let socket = null;
         onMounted(() => {
@@ -82,5 +91,10 @@ export default {
 </script>
 
 <style scoped>
-
+div.user-color {
+    text-align: center;
+    color: white;
+    font-size: 30px;
+    font-weight: 600;
+}
 </style>
