8926f6bdd03326368c62db54d6f78517787d0541
Your Name <you@example.com>
2023-03-14 21:09:45 +0800

    modify kob.sql

--- backendcloud/kob.sql
+++ backendcloud/kob.sql
@@ -1,45 +1,48 @@
+-- we don't know how to generate root <with-no-name> (class Root) :(
 create table bot
 (
-    id          int auto_increment
-        primary key,
-    user_id     int not null,
-    title       varchar(200),
-    description varchar(500),
-    content     varchar(10000),
-    rating      int default 1200,
-    create_time datetime,
-    modify_time datetime,
+    id          int auto_increment,
+    user_id     int              not null,
+    title       varchar(100)     null,
+    description varchar(300)     null,
+    content     varchar(10000)   null,
+    rating      int default 1500 null,
+    createtime  datetime         null,
+    modifytime  datetime         null,
     constraint table_name_id_uindex
         unique (id)
-);
+)
+    engine = InnoDB;
+
+alter table bot
+    add primary key (id);
 
 create table record
 (
-    id          int auto_increment
-            primary key,
-    a_id        int,
-    a_sx        int,
-    a_sy        int,
-    b_id        int,
-    b_sx        int,
-    b_sy        int,
-    a_steps     varchar(1500),
-    b_steps     varchar(1500),
-    map         varchar(1500),
-    loser       varchar(15),
-    create_time datetime,
-    constraint record_id_uindex
-    unique (id)
-);
+    id         int auto_increment
+        primary key,
+    a_id       int           null,
+    a_sx       int           null,
+    a_sy       int           null,
+    b_id       int           null,
+    b_sx       int           null,
+    b_sy       int           null,
+    a_steps    varchar(1000) null,
+    b_steps    varchar(1000) null,
+    map        varchar(1000) null,
+    loser      varchar(10)   null,
+    createtime datetime      null
+)
+    engine = InnoDB;
 
 create table user
 (
     id       int auto_increment
-    primary key,
-    username varchar(100),
-    password varchar(100),
-    rating   int default 1200,
-    photo    varchar(1000),
-    constraint user_id_uindex
-    unique (id)
-);
+        primary key,
+    username varchar(100)     null,
+    password varchar(100)     null,
+    photo    varchar(1000)    null,
+    rating   int default 1500 null
+)
+    engine = InnoDB;
+
